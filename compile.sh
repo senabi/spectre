8779db631d9a0eedc83886241f58f17763331e29
senabi <sensenabi@gmail.com>
2021-09-14 00:27:16 -0500

    update

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-clang++ -O3 MeasureReadLatency_x86_64.S spectre_safeside.cc -o spectre_pht
+clang++ -O3 read_latency.S spectre.cc -o spectre_pht
 #cc -lstdc++ MeasureReadLatency_x86_64.S spectre_safeside.cc -o spectre_pht
